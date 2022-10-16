c97b8449cb2d99c9605bd14c3436fd6ce324e6c5
Artem Makogon <artem.makogon.nsk@mail.ru>
2022-10-16 10:34:59 +0300

    init icpc

---
+++ ICPC/1-8-2022/1.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    cout << n * n * n << endl;
+    return 0;
+}
